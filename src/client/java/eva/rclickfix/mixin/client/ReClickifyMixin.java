61902aca5436592b52ff7b9a4890f43f5fcc60f5
EvarikaArkana <evarikaarkana@proton.me>
2025-05-12 12:40:19 -0400

    removed unused imports

--- src/client/java/eva/rclickfix/mixin/client/ReClickifyMixin.java
+++ src/client/java/eva/rclickfix/mixin/client/ReClickifyMixin.java
@@ -10,9 +10,7 @@ import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static com.mojang.text2speech.Narrator.LOGGER;
 import static net.minecraft.world.InteractionHand.OFF_HAND;
